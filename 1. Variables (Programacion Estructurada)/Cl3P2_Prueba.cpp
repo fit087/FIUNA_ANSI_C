10aa031b0907263d42da7b3f35c57ffeb5b28486
Adolfo Correa <fitomillennium@hotmail.com>
2015-11-06 15:35:31 -0200

    For loop test

---
+++ 1. Variables (Programacion Estructurada)/Cl3P2_Prueba.cpp
@@ -0,0 +1,10 @@
+// Programa de prueba de sentencias iterativas
+#include<iostream>
+using namespace std;
+int main()
+{   int i;
+    for ( i = 10; i > 0; i -= 3 )
+        cout << "\ni = " << i;
+    cout << "\n";
+    system ("pause");
+}
